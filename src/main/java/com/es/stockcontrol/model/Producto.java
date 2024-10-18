491932128077ae964bdd341c27f18d466aa6bd01
JJ <jujocave97@gmail.com>
2024-10-18 19:12:35 +0200

    clases producto y proveedor relacionadas

--- src/main/java/com/es/stockcontrol/model/Producto.java
+++ src/main/java/com/es/stockcontrol/model/Producto.java
@@ -1,4 +1,124 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    private String id;
+
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precioSinIva")
+    private float precioSinIva;
+
+    @Column(name = "precioConIva")
+    private float precioConIva;
+
+    @Column(name="fechaAlta")
+    private Date fechaAlta;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor_id")
+    private Proveedor proveedor;
+
+    public Producto(){
+
+    }
+
+    public Producto(String nombre, String categoria, String descripcion, float precioSinIva, float precioConIva, Date fechaAlta, int stock){
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precioConIva = precioConIva;
+        this.precioSinIva = precioSinIva;
+        this.fechaAlta = fechaAlta;
+        this.stock = stock;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecioSinIva() {
+        return precioSinIva;
+    }
+
+    public void setPrecioSinIva(float precioSinIva) {
+        this.precioSinIva = precioSinIva;
+    }
+
+    public float getPrecioConIva() {
+        return precioConIva;
+    }
+
+    public void setPrecioConIva(float precioConIva) {
+        this.precioConIva = precioConIva;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
 }
--- src/main/java/com/es/stockcontrol/model/Proveedor.java
+++ src/main/java/com/es/stockcontrol/model/Proveedor.java
@@ -1,4 +1,65 @@
 package com.es.stockcontrol.model;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "proveedores")
+
 public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @OneToMany(mappedBy = "proveedor")
+    private Set<Producto> productos;
+
+    public Proveedor(){
+
+    }
+
+    public Proveedor(String nombre, String direccion, Set<Producto> productos){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.productos = productos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Set<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Set<Producto> productos) {
+        this.productos = productos;
+    }
 }
