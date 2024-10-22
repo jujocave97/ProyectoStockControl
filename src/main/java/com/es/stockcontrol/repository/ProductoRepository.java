510b96c1c6d5325eefcfcceec89c9ec5801b4f2d
Jaregonz <jaregonz.jr@gmail.com>
2024-10-22 20:44:19 +0200

    Se agregan los metodos a la clase ProductoRepository

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -1,4 +1,74 @@
 package com.es.stockcontrol.repository;
 
-public class ProductoRepository {
+import com.es.stockcontrol.model.Producto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.Date;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class ProductoRepository{
+    private EntityManager entityManager;
+
+    public ProductoRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Producto getProducto(String id) {
+        this.entityManager.getTransaction().begin();
+        Producto productoBusqueda = this.entityManager.find(Producto.class,id);
+        this.entityManager.close();
+        return productoBusqueda;
+    }
+
+    public Producto insertProducto(Producto newProducto) {
+        this.entityManager.getTransaction().begin();
+        this.entityManager.persist(newProducto);
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+        return getProducto(newProducto.getId());
+    }
+
+    public Producto modificarNombreProducto(String id, String nuevoNombre){
+        this.entityManager.getTransaction().begin();
+        Producto productoBusqueda = this.entityManager.find(Producto.class,id);
+        productoBusqueda.setNombre(nuevoNombre);
+        this.entityManager.getTransaction().commit();
+        return getProducto(id);
+    }
+
+    public Producto modificarStockProducto(String id, String nuevoStock){
+        this.entityManager.getTransaction().begin();
+        Producto productoBusqueda = this.entityManager.find(Producto.class,id);
+        productoBusqueda.setStock(parseInt(nuevoStock));
+        this.entityManager.getTransaction().commit();
+        return getProducto(id);
+    }
+
+    public List<Producto> getProductosConStock() {
+        this.entityManager.getTransaction().begin();
+        String jpql = "SELECT p FROM Producto p WHERE p.stock =! 0";
+        TypedQuery<Producto> query = this.entityManager.createQuery(jpql, Producto.class);
+        this.entityManager.close();
+        return query.getResultList();
+    }
+    public List<Producto> getProductosSinStock() {
+        this.entityManager.getTransaction().begin();
+        String jpql = "SELECT p FROM Producto p WHERE p.stock = 0";
+        TypedQuery<Producto> query = this.entityManager.createQuery(jpql, Producto.class);
+        this.entityManager.close();
+        return query.getResultList();
+    }
+
+    public void eliminarProducto(String id){
+        this.entityManager.getTransaction().begin();
+        this.entityManager.remove(getProducto(id));
+        this.entityManager.getTransaction().commit();
+        this.entityManager.close();
+
+    }
+
 }
